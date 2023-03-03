14845c5b44a1445d4f2adceea777936ce5946fa8
emi-lex <emilexxxxx@gmail.com>
2023-03-03 13:34:31 +0100

    methylimp

---
+++ code/methyLImp.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.special import expit
+
+def methyLImp(M):
+    M_prime = M.copy()
+    R = set(range(M.shape[0]))
+    C = set(range(M.shape[1]))
+    NA = set(c for c in range(M.shape[1]) if np.any(np.isnan(M[:,c])))
+    L = NA.copy()
+    while len(L) > 0:
+        col = L.pop()
+        R_NA = set(r for r in range(M.shape[0]) if np.isnan(M[r,col]))
+        C_NA = set(c for c in range(M.shape[1]) if set(r for r in range(M.shape[0]) if np.isnan(M[r, c]))==R_NA)
+        if len(R - R_NA) > 0 and len(C - NA) > 0:
+            A = M[list(R - R_NA), list(C - NA)]
+            B = M[list(R - R_NA), col]
+            X = M[list(R_NA), list(C - NA)]
+            A_inv = np.linalg.pinv(A)
+            M_prime[list(R_NA), list(C_NA)] = expit(X @ A_inv @ np.log(B / (1 - B)))
+        L -= C_NA
+    return M_prime
