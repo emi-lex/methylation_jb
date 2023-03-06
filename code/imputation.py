b4a03596ae37bd470631ab987b06af4a3bedcc7d
emi-lex <emilexxxxx@gmail.com>
2023-03-06 13:11:45 +0100

    zeroes inputation

--- code/imputation.py
+++ code/imputation.py
@@ -3,9 +3,14 @@ import pandas as pd
 from missforest.miss_forest import MissForest
 from tqdm import tqdm
 
-def impute(X: np.ndarray, method="zeroes"):
+def impute(X: np.ndarray, head=None, method="zeroes"):
+    header = None
+    if not head is None:
+        header = '\t'.join(head)
     if method == "zeroes":
-        return np.nan_to_num(X, nan=0)
+        res = np.concatenate((X[:, 0][:, np.newaxis], np.nan_to_num(X[:, 1:].astype(np.float64), nan=0)), axis=1)
+        np.savetxt('../data/zeroes_imputed_cytosines.tsv', res, delimiter='\t', fmt='%s', header=header)
+        return res
     if method == "1000bp":
         chromosome, position = np.transpose([name.split(".") for name in X[:, 0]])
         position = position.astype(int)
@@ -13,8 +18,12 @@ def impute(X: np.ndarray, method="zeroes"):
             j += 1
             appropriate_rows = (chromosome == chromosome[i]) & (position > position[i] - 500) & (position < position[i] + 500)
             X[i, j] = X[appropriate_rows, j].mean()
-        return np.nan_to_num(X, nan=0)
+        res = np.concatenate((X[:, 0][:, np.newaxis], np.nan_to_num(X[:, 1:].astype(np.float64), nan=0)), axis=1)
+        np.savetxt('../data/1000bp_imputed_cytosines.tsv', res, delimiter='\t', fmt='%s', header=header)
+        return res
     if method == "missForest":
         imputer = MissForest()
         X_imputed = imputer.fit_transform(pd.DataFrame(X[:, 1:]))
-        return np.concatenate((X[:, 0], X_imputed), axis=1)
+        res = np.concatenate((X[:, 0], X_imputed), axis=1)
+        np.savetxt('../data/missForest_imputed_cytosines.tsv', res, delimiter='\t', fmt='%s', header=header)
+        return res
