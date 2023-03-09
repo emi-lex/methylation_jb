4a0a2a467b76e1c934c0c57b3da514d03133edfb
emi-lex <emilexxxxx@gmail.com>
2023-03-09 15:10:33 +0100

    first try to apply clocks

--- code/imputation.py
+++ code/imputation.py
@@ -1,29 +1,58 @@
 import numpy as np
 import pandas as pd
-from missforest.miss_forest import MissForest
+# from missforest.miss_forest import MissForest
+import missForest
 from tqdm import tqdm
 
 def impute(X: np.ndarray, head=None, method="zeroes"):
     header = None
     if not head is None:
         header = '\t'.join(head)
+        
     if method == "zeroes":
         res = np.concatenate((X[:, 0][:, np.newaxis], np.nan_to_num(X[:, 1:].astype(np.float64), nan=0)), axis=1)
         np.savetxt('../data/zeroes_imputed_cytosines.tsv', res, delimiter='\t', fmt='%s', header=header)
         return res
+    
     if method == "1000bp":
         chromosome, position = np.transpose([name.split(".") for name in X[:, 0]])
         position = position.astype(int)
+
+        def compare_chr(chr1, p1, chr2, p2):
+            return chr1 < chr2 or (chr1 == chr2 and p1 < p2)
+
+        def binary_search(c, p):
+            left = 0
+            right = len(chromosome)
+            while right - left > 1:
+                mid = (right + left) // 2
+                if compare_chr(c, p, chromosome[mid], position[mid]):
+                    right = mid
+                else:
+                    left = mid
+            return left
+
+
         for i, j in tqdm(np.argwhere(np.isnan(X[:, 1:].astype(float)))):
             j += 1
-            appropriate_rows = (chromosome == chromosome[i]) & (position > position[i] - 500) & (position < position[i] + 500)
-            X[i, j] = X[appropriate_rows, j].mean()
+            left_row = binary_search(chromosome[i], position[i] - 500)
+            right_row = binary_search(chromosome[i], position[i] + 500)
+            if chromosome[left_row] != chromosome[i] or position[left_row] != position[i] - 500:
+                left_row += 1
+            if right_row < left_row:
+                print(right_row, left_row, i, compare_chr(chromosome[i], position[i], chromosome[right_row], position[right_row]))
+            X[i, j] = X[left_row:right_row + 1, j].mean()
         res = np.concatenate((X[:, 0][:, np.newaxis], np.nan_to_num(X[:, 1:].astype(np.float64), nan=0)), axis=1)
         np.savetxt('../data/1000bp_imputed_cytosines.tsv', res, delimiter='\t', fmt='%s', header=header)
         return res
+    
     if method == "missForest":
-        imputer = MissForest()
-        X_imputed = imputer.fit_transform(pd.DataFrame(X[:, 1:]))
-        res = np.concatenate((X[:, 0], X_imputed), axis=1)
-        np.savetxt('../data/missForest_imputed_cytosines.tsv', res, delimiter='\t', fmt='%s', header=header)
-        return res
+        imputer = missForest.MissForest()
+        # return pd.DataFrame(X)
+        X_imputed = imputer.fit_transform(pd.DataFrame(X))
+        np.savetxt('../data/missForest_imputed_cytosines.tsv', X_imputed, delimiter='\t', fmt='%s', header=header)
+        return X_imputed
+        # X_imputed = imputer.fit_transform(pd.DataFrame(X[:, 1:]))
+        # res = np.concatenate((X[:, 0], X_imputed), axis=1)
+        # np.savetxt('../data/missForest_imputed_cytosines.tsv', res, delimiter='\t', fmt='%s', header=header)
+        # return res
--- code/methyLImp.py
+++ code/methyLImp.py
@@ -12,10 +12,13 @@ def methyLImp(M):
         R_NA = set(r for r in range(M.shape[0]) if np.isnan(M[r,col]))
         C_NA = set(c for c in range(M.shape[1]) if set(r for r in range(M.shape[0]) if np.isnan(M[r, c]))==R_NA)
         if len(R - R_NA) > 0 and len(C - NA) > 0:
-            A = M[list(R - R_NA), list(C - NA)]
-            B = M[list(R - R_NA), col]
-            X = M[list(R_NA), list(C - NA)]
+            A = M[list(R - R_NA)][:, list(C - NA)]
+            B = M[list(R - R_NA)][:, list(C_NA)]
+            X = M[list(R_NA)][:, list(C - NA)]
             A_inv = np.linalg.pinv(A)
-            M_prime[list(R_NA), list(C_NA)] = expit(X @ A_inv @ np.log(B / (1 - B)))
+            for row, values in zip(list(R_NA), expit(X @ A_inv @ np.log((B + 10e-06) / (1 - B))).reshape(len(R_NA), len(C_NA))):
+                for col, val in zip(list(C_NA), values):
+                    M_prime[row, col] = val
+            # M_prime[list(R_NA)][:, list(C_NA)] = expit(X @ A_inv @ np.log(B / (1 - B)))
         L -= C_NA
     return M_prime
